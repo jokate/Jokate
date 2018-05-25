8465dbf3dcd6653f476fed6fbcf3bbba95b6f840
jokate <kkkk4017@naver.com>
2018-05-25 20:04:58 +0900

    First Commit

---
+++ johncena.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main (void ) {
+	printf("AND HIS NAME IS JOOOOOOOOOOOOHN CEEEEEEEEEEEEENAAAAAAAAAAAAAAAAAAAAAAA");
+	return 0;
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main (void) {
+	printf("HELLNO \n");
+	return 0;
+}
